e320ac61b2c9582baf99d400ff020732202cb33b
shge <25113178+shge@users.noreply.github.com>
2019-03-10 19:58:51 +0900

    Change footer

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(function() {
 
 // Navbar
 $('#nav').load('/nav.html?201804062022');
+$('#footer').load('/footer.html?201903101955');
 $('.navbar-burger').on('click', function(){
   $('.navbar-burger, .navbar-menu').toggleClass('is-active');
 });
